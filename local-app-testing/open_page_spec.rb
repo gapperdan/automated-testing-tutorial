31d0e1939719ee6da43012ccfe66032a5ee66d9f
Danny Tizon <dtizon@gmail.com>
2013-10-23 21:10:17 -0700

    Updated form

--- local-app-testing/open_page_spec.rb
+++ local-app-testing/open_page_spec.rb
@@ -1,17 +1,28 @@
 require 'rubygems'
 require 'selenium-webdriver'
 require 'rspec'
-require "rspec-expectations"
 
-describe "local app test" do
+describe "login to my app" do
 
   TEST_URL = "http://localhost:4567"
   browser = Selenium::WebDriver.for :firefox
 
-  it "should open the login page" do
+  it "should open the browser to the login page" do
     browser.get TEST_URL  #browser.navigate.to TEST_URL
     browser.title.should == "Login page"
+  end
 
+  it "should log me in" do
+    userid = browser.find_element(:name, 'userid')
+    userid.send_keys "12345678"
+
+    password = browser.find_element(:name, 'password')
+    password.send_keys "secret"
+
+    password.submit
+  end
+
+  it "should close the browser" do
     browser.quit
   end
 
